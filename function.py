28d957647e72735bd0bfbce2be02bc040670ab0f
Andy <b10802101@gapps.ntust.edu.tw>
2023-09-04 21:26:08 +0800

    9/4 21:26

---
+++ function.py
@@ -0,0 +1,16 @@
+#people is the default value of name
+# def hello(greeting, name = "people"):
+#     print("Hi," "{}, {}".format(greeting, name))
+# hello("123")
+
+# #this function can receive values and keywords
+# def student(*args, **kwargs):    #arguments in function is changable
+#     print(args)
+#     print(kwargs)
+# student(1, 2, 3, age = 10, name = "John")
+
+# * can unpack the list, ** can unpack the dictionary
+def student(*args, **kwargs):
+    print(args)
+    print(kwargs)
+student(*([1,2,3]),**( {"name" : "andy", "age" : 10}))
