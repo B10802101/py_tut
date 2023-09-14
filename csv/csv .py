ecdacef1b3c9d45ccd20b13d25767d8e490cdd31
Andy <b10802101@gapps.ntust.edu.tw>
2023-09-14 12:56:45 +0800

    9/14 2

--- csv/csv .py
+++ csv/csv .py
@@ -1,20 +1,21 @@
 import csv
 import os
 import requests
+
 ################################################################Commands
-print(os.getcwd())  
-os.chdir('./csv')                                                            #change directory
-print(os.getcwd())                                                           #get current directory
-# print(os.path.exists('./list.py'))                                             #check if a file exist
+
+# os.chdir('./csv')                                                            #change directory
+# print(os.getcwd())                                                           #get current directory
+# print(os.path.exists('./list.py'))                                           #check if a file exist
 # os.path.isdir & os.path.isfile                                               #the path is a directory or a file
 # print(os.path.splitext('/Users/WD/source/repos/B10802101/py_tut/file.py'))   #split file name and extension into a tuple
 # print(os.listdir())                                                          #list all the files in a directory into a tuple
 ################################################################
 
-# os.chdir('./csv.py')
+# os.chdir('./csv')
 # with open('example.csv', 'r') as file_name:                                                 #file_name is a variable of the file example.csv
 #     csv_reader = csv.DictReader(file_name)                                                  #csv_reader is a register for reading file in Dict type
-#     with open('example_copy', 'w') as copy_file:                                            #copy_file is a variable of the file example_copy
+#     with open('example_copy.csv', 'w') as copy_file:                                            #copy_file is a variable of the file example_copy
 #         field_name = ["QuotaAmount", "StartDate", "OwnerName", "Username"]                  #accessing the class in origin csv file             
 #         csv_writer = csv.DictWriter(copy_file, fieldnames = field_name, delimiter = '\t')   #csv_writer is a register for writing file in Dict type
 #         for line in csv_reader:
@@ -24,10 +25,10 @@ print(os.getcwd())                                                           #ge
 
 ################################################################################################
 
-# with open('example.csv', 'w') as f:
-#     file_name, file_ext = os.path.splitext(".\csv.py\example.csv")
-#     new_file_name = file_name
-#     os.rename('.\csv.py\example.csv', new_file_name)
+# with open('ML_practice.py', 'r') as f:
+#     file_name, file_ext = os.path.splitext(f.name)
+#     new_file_name = file_name + '_9/14' + file_ext
+#     os.rename(f.name, new_file_name)
 
 ################################################################################################
 
@@ -45,11 +46,11 @@ print(os.getcwd())                                                           #ge
 
 ###########context manager######################################################################
 
-# with open("example.csv",'r') as f:
+# with open("list.py",'r') as f:
 #     pass
 # print(f.closed)
 
-# f = open('example.csv')
+# f = open('list.py')
 # print(f.name)
 # print(f.closed)
 # f.close()
@@ -57,21 +58,20 @@ print(os.getcwd())                                                           #ge
 
 ###########Read a file######################################################################
 
-# os.chdir('./csv.py')
+# os.chdir('./csv')
 # print(os.getcwd())
 # with open("read_a_file.txt",'r') as f:
-#     f_content = f.read(10)
+#     f_content = f.read(3)
 #     while len(f_content) > 0:
 #         print(f_content, end = '')
-#         f_content = f.read(10)
-#         #f.seek(0)
+#         f_content = f.read(3)
 
 ###########Copy a image file######################################################################
-# os.chdir('./csv.py')
-# with open("image.jpg",'rb') as fr:
-#     with open("image_copy.jpg",'wb') as fw:
-#         chunksize = 300
-#         fr_chunkcontent = fr.read(chunksize)
-#         while len(fr_chunkcontent) > 0:
-#             fw.write(fr_chunkcontent)                   #seize fr_chunkcontent to write into fw
-#             fr_chunkcontent = fr.read(chunksize)
+os.chdir('./csv')
+with open("image.jpg",'rb') as fr:
+    with open("image_copy.jpg",'wb') as fw:
+        chunksize = 300
+        fr_chunkcontent = fr.read(chunksize)
+        while len(fr_chunkcontent) > 0:
+            fw.write(fr_chunkcontent)                   #seize fr_chunkcontent to write into fw
+            fr_chunkcontent = fr.read(chunksize)
